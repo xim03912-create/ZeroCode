44ec503d821761b35216f4f1f1ecde0bd272a771
nishiguchi <masato.yuzu32104@icloud.com>
2026-02-02 13:04:58 +0900

    Push①

---
+++ HelloWorld_NishiguchiAkari/src/sample/SelfIntroduction.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		// 氏名
+		String name = "西口 明李";
+		// 年齢
+		int age = 24;
+		// 趣味
+		String hobby = "食べること、寝ること";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "です。");
+		System.out.println("趣味は" + hobby + "です。");
+				
+	}
+
+}
