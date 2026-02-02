38f424de091b2c2ec4077dbc343a352d02f8ca17
nishiguchi <masato.yuzu32104@icloud.com>
2026-02-02 13:22:45 +0900

    push①

--- HelloWorld_NishiguchiAkari/src/sample/SelfIntroduction.java
+++ HelloWorld_NishiguchiAkari/src/sample/SelfIntroduction.java
@@ -11,11 +11,9 @@ public class SelfIntroduction {
 		int age = 24;
 		// 趣味
 		String hobby = "食べること、寝ること";
-		
 		/*
 		* 自己紹介文を出力する処理
 		*/
-		
 		System.out.println("私の名前は" + name + "です。");
 		System.out.println("年齢は" + age + "です。");
 		System.out.println("趣味は" + hobby + "です。");
