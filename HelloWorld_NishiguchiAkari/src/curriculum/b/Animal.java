50f582fdd1a2c415f9d1ae71bfd68b17cc0370c5
nishiguchi <masato.yuzu32104@icloud.com>
2026-02-19 23:09:36 +0900

    Question5 初回コミット

---
+++ HelloWorld_NishiguchiAkari/src/curriculum/b/Animal.java
@@ -0,0 +1,35 @@
+package curriculum.b;
+
+public class Animal {
+
+	private String name;
+	private double length;
+	private int speed;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+	public void setLength(double length) {
+		this.length = length;
+	}
+	public double getLength() {
+		return length;
+	}
+	
+	
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	
+	
+	
+
+}
---
+++ HelloWorld_NishiguchiAkari/src/curriculum/b/Greeting.java
@@ -0,0 +1,23 @@
+package curriculum.b;
+
+public class Greeting {
+	
+	
+	
+	public static void sayHello() { //Q4：メソッドの定義
+			
+			System.out.println("こんにちは！");
+			
+   }
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
