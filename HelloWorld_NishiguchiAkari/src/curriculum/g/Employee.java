9060c4504c1277837501935a8f0dc22842bd09a8
nishiguchi <masato.yuzu32104@icloud.com>
2026-02-26 23:25:20 +0900

    curriculum.g プッシュ①

---
+++ HelloWorld_NishiguchiAkari/src/curriculum/g/Employee.java
@@ -0,0 +1,44 @@
+package curriculum.g;
+
+public class Employee {
+
+	private static String employeed; //employeeId（文字列型）
+	private static String name; //name（文字列型）
+	
+	public void setEmployeeId(String id) {
+		
+		this.employeed = id;
+		
+	}
+	
+	public void setName(String name) {
+		
+		this.name = name;
+		
+	}
+	
+	
+	public String getEmployeeId() {
+		return employeed;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Employee employee = new Employee ();
+		
+		employee.setEmployeeId("E002");
+		employee.setName("田中花子");
+		
+		String id  = employee.getEmployeeId();
+		String n  = employee.getName();
+		
+		System.out.println("社員ID：" + id + "," + "名前：" + n);
+	} //----------------------------------------------
+
+}//------------------------------------------------
