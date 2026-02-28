615b977504a9696bdc96de84488d66a8817aba60
nishiguchi <masato.yuzu32104@icloud.com>
2026-03-01 00:25:32 +0900

    curriculum.h プッシュ①

---
+++ HelloWorld_NishiguchiAkari/src/curriculum/h/Employee.java
@@ -0,0 +1,86 @@
+package curriculum.h;
+
+public abstract class Employee {
+
+	protected String employeed;
+	protected String name;
+	
+	
+	public Employee(String id, String name) { //コンストラクタ
+		
+		this.employeed = id;
+		this.name = name;
+		
+	}
+	
+	
+	public abstract int calculateDailyWage(int hoursWorked); //メソッド
+	
+	
+}
+    class FullTimeEmployee extends Employee {
+		
+		private static final int HOURLY_RATE = 1200; //定数
+		
+		public FullTimeEmployee(String id, String name) {
+			super(id, name);
+			// TODO 自動生成されたコンストラクター・スタブ
+		}
+		
+		@Override
+		public int calculateDailyWage(int hoursWorked) { 
+			
+			if (hoursWorked <= 8) {
+				return hoursWorked * HOURLY_RATE;
+			} else {
+				int regularPay = 8 * HOURLY_RATE;
+				int overtimeHours = hoursWorked - 8;
+				
+				int overtimePay = (int) (overtimeHours * HOURLY_RATE * 1.25);
+				return regularPay + overtimePay;
+			}
+		}
+	
+	}
+    
+	class PartTimeEmployee extends Employee {//---
+			
+		private static final int HOURLY_RATE = 1000; //定数
+			
+		public PartTimeEmployee(String id, String name) {
+			super(id, name);
+			// TODO 自動生成されたコンストラクター・スタブ
+		}
+	 
+		@Override
+		public int calculateDailyWage(int hoursWorked) { 
+			
+			return hoursWorked * HOURLY_RATE;
+		}
+		
+	
+	}//---
+	
+   class Main {
+	   
+	   public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		
+		FullTimeEmployee ft = new FullTimeEmployee("F001","正社員A");
+		
+		PartTimeEmployee pt = new PartTimeEmployee("P001","パート社員B");
+		
+		
+		int hours = 9;
+		
+		System.out.println("正社員の給与" + ft.calculateDailyWage(hours) + "円");
+		System.out.println("パート社員の給与" + pt.calculateDailyWage(hours) + "円");
+		
+	}//------------------------------------------
+	
+
+
+}//--------------------------------------------------
+
+
