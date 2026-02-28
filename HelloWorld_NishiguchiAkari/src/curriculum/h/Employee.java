75fbe766dea5192622bc36cb53cfbfa40085237d
nishiguchi <masato.yuzu32104@icloud.com>
2026-03-01 00:31:09 +0900

    curriculum.h プッシュ②

--- HelloWorld_NishiguchiAkari/src/curriculum/h/Employee.java
+++ HelloWorld_NishiguchiAkari/src/curriculum/h/Employee.java
@@ -1,6 +1,6 @@
 package curriculum.h;
 
-public abstract class Employee {
+abstract class Employee {
 
 	protected String employeed;
 	protected String name;
@@ -61,26 +61,6 @@ public abstract class Employee {
 	
 	}//---
 	
-   class Main {
-	   
-	   public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-		
-		FullTimeEmployee ft = new FullTimeEmployee("F001","正社員A");
-		
-		PartTimeEmployee pt = new PartTimeEmployee("P001","パート社員B");
-		
-		
-		int hours = 9;
-		
-		System.out.println("正社員の給与" + ft.calculateDailyWage(hours) + "円");
-		System.out.println("パート社員の給与" + pt.calculateDailyWage(hours) + "円");
-		
-	}//------------------------------------------
 	
-
-
-}//--------------------------------------------------
 
 
---
+++ HelloWorld_NishiguchiAkari/src/curriculum/h/Main.java
@@ -0,0 +1,19 @@
+package curriculum.h;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+FullTimeEmployee ft = new FullTimeEmployee("F001","正社員A");
+		
+		PartTimeEmployee pt = new PartTimeEmployee("P001","パート社員B");
+		
+		
+		int hours = 9;
+		
+		System.out.println("正社員の給与" + ft.calculateDailyWage(hours) + "円");
+		System.out.println("パート社員の給与" + pt.calculateDailyWage(hours) + "円");
+	}
+
+}
