8e9046c4adccf609e560655009c99b4f99c8e7ac
nishiguchi <masato.yuzu32104@icloud.com>
2026-03-04 22:37:48 +0900

    curriculum.k プッシュ①

---
+++ HelloWorld_NishiguchiAkari/src/curriculum/k/Main.java
@@ -0,0 +1,23 @@
+package curriculum.k;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		SalaryReporter reporter = new SalaryReporter();
+
+        // ポリモーフィズムにより、異なる型の社員を同じ Payable として扱える
+        Payable[] employees = {
+            new FullTimeEmployee("田中", 160),
+            new ContractEmployee("佐藤", 140),
+            new PartTime("山田", 140)
+        };
+
+        for (Payable e : employees) {
+            reporter.report(e);
+        }
+        
+	}
+
+}
---
+++ HelloWorld_NishiguchiAkari/src/curriculum/k/SalaryCalculator.java
@@ -0,0 +1,131 @@
+package curriculum.k;
+
+/*class SalaryCalculator { //SalaryCalculator は 依存を抽象化（インターフェイス/抽象クラス） する
+	
+	public void calculateSalary(Employee e) {
+		
+		
+		if (e.getType().equals("FullTime")) {
+			System.out.println(e.getName() + " の給料は " + (e.getHours() * 1200) + " 円");
+		}else if (e.getType().equals("Contract")) {
+			
+		}System.out.println(e.getName() + " の給料は " + (e.getHours() * 1000) + " 円");
+	}
+	
+	
+}//-----------------------------------------------------
+
+class Employee { //Employee に型情報を持たせるのではなく、ポリモーフィズムを活用！
+	private String name;
+	private String type;
+	private int hours;
+	
+	public Employee(String name, String type, int hours) {
+		this.type = type;
+		this.hours = hours;
+	}
+	
+	public String getName() { return name; }
+	public String getType() { return type; }
+	public int getHours() { return hours; }
+	
+
+}//-------------------------------------------------------
+
+*/
+
+
+	
+	interface Payable { // インターフェイス
+		
+		int calculateSalary();
+
+		String getName();
+	}
+
+	abstract class Employee implements Payable { //抽象クラス
+		private String name;
+		private String type;
+		private int hours;
+	}
+	
+    // FullTimeEmployee
+	class FullTimeEmployee implements Payable { // 計算ルールを変えたい場合 S
+		private String name;
+		private int hours;
+		
+		public FullTimeEmployee(String name, int hours) {
+			this.name =name;
+			this.hours = hours;
+		}
+		@Override
+		public String getName() {
+			return name;
+		}
+		@Override
+	    public int calculateSalary() {
+	        return hours * 1200; 
+	    }
+	}
+	
+	// ContractEmployee
+	class ContractEmployee implements Payable { // 計算ルールを変えたい場合 S
+	    private final String name;
+	    private final int hours;
+
+	    public ContractEmployee(String name, int hours) {
+	        this.name = name;
+	        this.hours = hours;
+	    }
+	    
+	    @Override
+	    public String getName() {
+	        return name;
+	    }
+
+	    @Override
+	    public int calculateSalary() {
+	        return hours * 1000;
+	    }
+	}
+	
+	class PartTime implements Payable { // 新たな社員タイプを追加
+	    private final String name;
+	    private final int hours;
+
+	    public PartTime(String name, int hours) {
+	        this.name = name;
+	        this.hours = hours;
+	    }
+	    
+	    @Override
+	    public String getName() {
+	        return name;
+	    }
+
+	    @Override
+	    public int calculateSalary() {
+	        return hours * 1000;
+	    }
+	}
+	
+	
+
+	class SalaryReporter { //表示形式を変えたい場合
+		public void report(Payable payable) {
+			
+			System.out.println(payable.getName() + "さんの給料は " + payable.calculateSalary() + " 円");
+			
+		}
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
