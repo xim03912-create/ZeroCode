eb48b62385b02d945b4276faee91cfd306938f94
nishiguchi <masato.yuzu32104@icloud.com>
2026-03-27 01:07:21 +0900

    【※提出必須】自己紹介プログラムを作成 初回コミット

---
+++ HelloWorld_NishiguchiAkari/src/self_introduction/Main.java
@@ -0,0 +1,61 @@
+package self_introduction;
+
+class Person {
+	// インスタンスフィールドを定義
+	String name;
+	int age;
+	double height;
+	double weight;
+	double bmi;
+	
+	static int count = 0;
+	
+	
+	 // コンストラクタを定義しインスタンスフィールドに値をセット
+	 Person(String n, int age, double h, double w){
+		 
+		 this.name = n;
+		 this.age = age;
+		 this.height = h;
+	     this.weight = w;
+	     
+	     
+	     Person.count++;
+	 }
+	 
+	 //インスタンスメソッドbmiの定義
+	 
+	 public double bmi() {
+		 
+		 return this.weight / (this.height * this.height);
+	 }
+	 
+	 public void print() {
+		 System.out.println("名前は" + this.name + "です");
+		 System.out.println("年は" + this.age + "です");
+		 System.out.println("BMIは" + this.bmi() + "です");
+	 }
+	 
+	 public static void printCount() {
+	        System.out.println("合計" + Person.count + "人です");
+	 }
+	 
+}
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60.0);
+	    
+	    person1.print();
+	    
+	    Person.printCount();
+	}
+	
+	
+
+}
